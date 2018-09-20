472326ff53eaffc6ce4ad751935eb00c3db50111
Joshua Williamson <jlwilliamson95@gmail.com>
2018-09-20 15:10:30 -0600

    Changed vaiable to a shorter variable I didn't know about

--- SetDomain.ps1
+++ SetDomain.ps1
@@ -1,49 +1,49 @@
 Write-Host "Which canvas are you using:"
 $response = Read-Host "[1] Main, [2] Test, [3] MasterCourses"
 #If the CanvasApiCreds.json file exists this will find which domain it is for, copy it to the a different name and delete it. This is to ensure you don't accidently use the wrong ApiCreds with the wrong domain.
-if(Test-Path "$env:HOMEDRIVE$env:HOMEPATH\Documents\CanvasApiCreds.json"){
-  $CanvasType =  (Get-Content "$env:HOMEDRIVE$env:HOMEPATH\Documents\CanvasApiCreds.json" | ConvertFrom-Json).BaseUri
+if(Test-Path "$HOME\Documents\CanvasApiCreds.json"){
+  $CanvasType =  (Get-Content "$HOME\Documents\CanvasApiCreds.json" | ConvertFrom-Json).BaseUri
   switch ($CanvasType)
   {
     "https://byu.instructure.com"{
-      Copy-Item -Path "$env:HOMEDRIVE$env:HOMEPATH\Documents\CanvasApiCreds.json" -Destination "$env:HOMEDRIVE$env:HOMEPATH\Documents\BYU_CanvasApiCreds.json" -Force
+      Copy-Item -Path "$HOME\Documents\CanvasApiCreds.json" -Destination "$HOME\Documents\BYU_CanvasApiCreds.json" -Force
       break
     }
     "https://byuistest.instructure.com"{
-      Copy-Item -Path "$env:HOMEDRIVE$env:HOMEPATH\Documents\CanvasApiCreds.json" -Destination "$env:HOMEDRIVE$env:HOMEPATH\Documents\TEST_CanvasApiCreds.json" -Force
+      Copy-Item -Path "$HOME\Documents\CanvasApiCreds.json" -Destination "$HOME\Documents\TEST_CanvasApiCreds.json" -Force
       break
     }
     "https://byuismastercourses.instructure.com"{
-      Copy-Item -Path "$env:HOMEDRIVE$env:HOMEPATH\Documents\CanvasApiCreds.json" -Destination "$env:HOMEDRIVE$env:HOMEPATH\Documents\MASTER_CanvasApiCreds.json" -Force
+      Copy-Item -Path "$HOME\Documents\CanvasApiCreds.json" -Destination "$HOME\Documents\MASTER_CanvasApiCreds.json" -Force
       break
     }
     default{
-      Write-Host "CanvasApiCreds.json for unknown domain found, saving to `"$env:HOMEDRIVE$env:HOMEPATH\Documents\UNKNOWN_CanvasApiCreds.json`"" -ForegroundColor Red
-      Copy-Item -Path "$env:HOMEDRIVE$env:HOMEPATH\Documents\CanvasApiCreds.json" -Destination "$env:HOMEDRIVE$env:HOMEPATH\Documents\UNKNOWN_CanvasApiCreds.json" -Force
+      Write-Host "CanvasApiCreds.json for unknown domain found, saving to `"$HOME\Documents\UNKNOWN_CanvasApiCreds.json`"" -ForegroundColor Red
+      Copy-Item -Path "$HOME\Documents\CanvasApiCreds.json" -Destination "$HOME\Documents\UNKNOWN_CanvasApiCreds.json" -Force
     }
   }
-  Remove-Item -Path "$env:HOMEDRIVE$env:HOMEPATH\Documents\CanvasApiCreds.json"
+  Remove-Item -Path "$HOME\Documents\CanvasApiCreds.json"
 }
 
 switch ($response)
 {
   "1"{
-    if(Test-Path "$env:HOMEDRIVE$env:HOMEPATH\Documents\BYU_CanvasApiCreds.json"){
-      Copy-Item -Path "$env:HOMEDRIVE$env:HOMEPATH\Documents\BYU_CanvasApiCreds.json" -Destination "$env:HOMEDRIVE$env:HOMEPATH\Documents\CanvasApiCreds.json" -Force
+    if(Test-Path "$HOME\Documents\BYU_CanvasApiCreds.json"){
+      Copy-Item -Path "$HOME\Documents\BYU_CanvasApiCreds.json" -Destination "$HOME\Documents\CanvasApiCreds.json" -Force
     }else{
       Write-Host "You will need to create an Canvas API for this domain."
     }
     break
   }"2"{
-    if(Test-Path "$env:HOMEDRIVE$env:HOMEPATH\Documents\TEST_CanvasApiCreds.json"){
-      Copy-Item -Path "$env:HOMEDRIVE$env:HOMEPATH\Documents\TEST_CanvasApiCreds.json" -Destination "$env:HOMEDRIVE$env:HOMEPATH\Documents\CanvasApiCreds.json" -Force
+    if(Test-Path "$HOME\Documents\TEST_CanvasApiCreds.json"){
+      Copy-Item -Path "$HOME\Documents\TEST_CanvasApiCreds.json" -Destination "$HOME\Documents\CanvasApiCreds.json" -Force
     }else{
       Write-Host "You will need to create an Canvas API for this domain."
     }
     break
   }"3"{
-    if(Test-Path "$env:HOMEDRIVE$env:HOMEPATH\Documents\MASTER_CanvasApiCreds.json"){
-      Copy-Item -Path "$env:HOMEDRIVE$env:HOMEPATH\Documents\MASTER_CanvasApiCreds.json" -Destination "$env:HOMEDRIVE$env:HOMEPATH\Documents\CanvasApiCreds.json" -Force
+    if(Test-Path "$HOME\Documents\MASTER_CanvasApiCreds.json"){
+      Copy-Item -Path "$HOME\Documents\MASTER_CanvasApiCreds.json" -Destination "$HOME\Documents\CanvasApiCreds.json" -Force
     }else{
       Write-Host "You will need to create an Canvas API for this domain."
     }
