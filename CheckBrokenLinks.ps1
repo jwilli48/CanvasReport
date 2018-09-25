4bd68571d34fae5c326ca9ca761f46577fbfe955
Joshua Williamson <jlwilliamson95@gmail.com>
2018-09-25 14:26:47 -0600

    Prints where report is saved

--- CheckBrokenLinks.ps1
+++ CheckBrokenLinks.ps1
@@ -99,7 +99,7 @@ $data = Transpose-Data Location, URL, Status $Global:location, $Global:href, $Gl
 $Global:ExcelReport = $PSScriptRoot + "\Reports\LinkCheck_" + $courseName + ".xlsx"
 if(-not ($NULL -eq $data)){
   $data | Export-Excel $ExcelReport -AutoFilter -AutoSize -Append
-  Write-Host "Report saved to $PsScriptRoot$ExcelReport"
+  Write-Host "Report saved to $ExcelReport"
 }else{
   Write-Host "Nothing found" -ForegroundColor Green
 }
