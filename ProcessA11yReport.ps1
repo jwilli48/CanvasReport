f9714754a9ed525df5f4a86e729267e8c973bed5
Joshua Williamson <jlwilliamson95@gmail.com>
2018-10-11 17:57:23 -0600

    Cleaned up unused block of code

--- ProcessA11yReport.ps1
+++ ProcessA11yReport.ps1
@@ -47,30 +47,6 @@ function Start-ProcessLinks{
   }
 
   #$href_list = $link_list | Select-String -pattern 'href="(.*?)"' -AllMatches | ForEach-Object {$_.Matches.Groups[1].Value}
-
-  <#Checks broken links, not needed since Canvas has it built in
-  if($Global:CheckLinks -eq $NULL){
-    $host.ui.rawui.foregroundColor = "Yellow"
-    $Global:CheckLinks = Read-Host "Would you like to check for broken links? (Y/N)`nThis on average doubles the time to generate this report"
-    $host.ui.rawui.foregroundColor = "White"
-  }else{
-    if($Global:CheckLinks -match "Y"){
-      foreach($href in $href_list){
-        if($href.contains("mailto")){
-          #email link
-          continue
-        }
-        try{
-          Invoke-WebRequest $href | Out-Null
-        }catch{
-          #There is also a SecureChannelFailure error status but it seems that for the majority of cases those still work when clicking the link manually
-          if($_.Exception.Status -eq "SendFailure"){
-            AddToArray "Link" "$($item.url -split `"api/v\d/`" -join `"`")" "" $href "Broken link"
-          }
-        }
-      }
-    }
-  }#>
   
   $link_text = $link_list | Select-String -pattern '<a.*?>(.*?)</a>' -AllMatches | ForEach-Object {$_.Matches.Groups[1].Value}
   foreach($text in $link_text){
